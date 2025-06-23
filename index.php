b669c905dc5a5d3eaa7b4626a4127e2a270f5791
Dara18-bling <lucionaradnas@gmail.com>
2025-06-23 19:56:19 +0800

    New Commit Here

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php session_start()?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Polycium University</title>
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+
+<div style="position: fixed; width: 100%; height: 60px; background-color: #32508F; display: flex; align-items: center; justify-content: space-between; padding: 0 17px; box-sizing: border-box;" id="Navigation_bar">
+  <div style="display: flex; align-items: center;">
+      <img src="../../../Images/new.png" width="50px" alt="Logo" style="vertical-align: middle; margin-right: 10px;" />
+    <span style="font-size: 30px; color: #FFFFFF; ">POLYCIUM UNIVERSITY</span>
+  </div>
+
+  <nav style="display: flex; gap: 12px; font-family: Helvetica; color: #FFFFFF; margin-right: 30px;">
+    <a class="navigation" style="font-size: 17px; color: #FFFFFF; text-decoration: none; padding: 4px 10px;" href="#">ABOUT</a>
+    <a class="navigation" style="font-size: 17px; color: #FFFFFF; text-decoration: none; padding: 4px 10px;" href="../../Academics/Academics.html">ACADEMICS</a>
+    <a class="navigation" style="font-size: 17px; color: #FFFFFF; text-decoration: none; padding: 4px 10px;" href="index.html">ADMISSION</a>
+    <a class="navigation" style="font-size: 17px; color: #FFFFFF; text-decoration: none; padding: 4px 10px;" href="../../StudentLog/StudentLogin.html">STUDENT PORTAL</a>
+  </nav>
+</div>
+
+    <div class="container">
+      <div class="login-box">
+        <img src="../../../Images/new.png" alt="University Logo" class="logo"/>
+        <h2>LOG IN</h2>
+
+        <form method="POST" action="new_connect.php">
+			<input type="email" id="email" name="email" placeholder="Email" required class="input-field" />
+			<input type="password" id="password" name="password" placeholder="Enter your password" required class="input-field" />	
+          <button class="login-btn" name="submit_inps" type="submit">LOGIN</button>
+        </form>
+
+        <div class="register-link">
+          Don’t have allowance yet? <br>
+		  <a href="../sign up/index1.html">Register Here.</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+      Polycium University © 2024 All Rights Served .
+    </div>
+  </body>
+</html>
